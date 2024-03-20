0b4cc4df1b28402f604ad961553804e3d44c9e8a
Kyle Willmann <krwillmann@mail.fhsu.edu>
2024-03-19 20:24:13 -1000

    host

--- config/Database.php
+++ config/Database.php
@@ -1,6 +1,27 @@
 <?php
 class Database{
-        private $conn;
+    private $host = 'localhost';
+    private $port = '5432';
+    private $db_name = 'quotesdb';
+    private $username = 'postgres';
+    private $password = 'postgres';
+    private $conn;
+
+    public function connect() {
+        $this->conn = null;
+        $dsn = "pgsl:host={$this->host};port={$this->port};dbname={$this->db_name}";
+
+        try {
+            $this->conn = new PDO($dsn, $this->username, $this->password);
+
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        }catch(PDOException $e){
+            echo 'Connection Error: ' . $e->getMessage();
+        }
+        return $this->conn;
+    }
+}
+/*        private $conn;
         private $host;
         private $port;
         private $dbname;
@@ -31,4 +52,4 @@ class Database{
                 }
             }
         }
-    }
+    }*/
